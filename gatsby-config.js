607787c8b15c4759a57f647c778b9266774a2737
Toni Meuschke <toni.meuschke@googlemail.com>
2018-03-13 20:55:18 +0100

    Add Netlify cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,15 @@ module.exports = {
   siteMetadata: {
     title: 'Gatsby Default Starter',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-plugin-netlify-cms`,
+      options: {
+        // One convention is to place your Netlify CMS customization code in a
+        // `src/cms` directory.
+        modulePath: `${__dirname}/src/cms/cms.js`,
+      },
+    },
+  ]
 };
