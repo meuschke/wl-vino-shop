a8528267d51c1adc8d27802e67f474030a3d958b
Toni Meuschke <toni.meuschke@googlemail.com>
2018-03-14 16:49:23 +0100

    Add shopify query

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,5 +12,19 @@ module.exports = {
         modulePath: `${__dirname}/src/cms/cms.js`,
       },
     },
+    {
+      resolve: 'gatsby-source-shopify-storefront',
+      options: {
+        // Your Shopify instance name (e.g. 'shopify-store-name',
+        // if your shopify shop is located at https://shopify-store-name.myshopify.com/)
+        siteName: 'vinomore-test',
+        // Your Shopify Storefront API access token
+        // generated in the private apps section of your store admin.
+        // Refer to Shopify's Storefront API Documentation for more information
+        // https://help.shopify.com/api/storefront-api/getting-started
+        accessToken: 'd4d827b50bddf07a893ee70a8b8d0e68',
+        collectionQuery: `title:'Test Shop'`
+      },
+    }
   ]
 };
--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,64 @@
  */
 
  // You can delete this file if you're not using it
+
+const _ = require(`lodash`)
+const Promise = require(`bluebird`)
+const path = require(`path`)
+const slash = require(`slash`)
+
+exports.createPages = ({ graphql, boundActionCreators }) => {
+  const { createPage } = boundActionCreators
+  return new Promise((resolve, reject) => {
+    // The “graphql” function allows us to run arbitrary
+    // queries against the local Shopify graphql schema. Think of
+    // it like the site has a built-in database constructed
+    // from the fetched data that you can run queries against.
+
+    // ==== PAGES (SHOPIFY PRODUCTS) ====
+    graphql(
+    `
+      {
+        allShopifyProducts {
+          edges {
+            node {
+              id
+            }
+          }
+        }
+      }
+    `
+    )
+      .then(result => {
+        if (result.errors) {
+          console.log(result.errors)
+          reject(result.errors)
+        }
+
+        // Create product pages.
+        const pageTemplate = path.resolve('./src/pages/index.js')
+        // We want to create a detailed page for each
+        // lever node. We'll just use the ID for the slug.
+        _.each(result.data.allShopifyProducts.edges, edge => {
+          // Gatsby uses Redux to manage its internal state.
+          // Plugins and sites can use functions like "createPage"
+          // to interact with Gatsby.
+          createPage({
+            // Each page is required to have a `path` as well
+            // as a template component. The `context` is
+            // optional but is often necessary so the template
+            // can query data specific to each page.
+            path: `/${edge.node.id}/`,
+            component: slash(pageTemplate),
+            context: {
+              id: edge.node.id,
+            },
+          })
+        })
+      })
+      // ==== END PAGES ====
+
+      // resolve() must be called at the end so Gatsby knows that we're done adding pages.
+      .then(resolve())
+  })
+}
